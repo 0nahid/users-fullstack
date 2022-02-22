d55cdf1bed4016442f571da67ca04d6ad6eeb291
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-22 22:13:57 +0600

    connected to mongo & send data to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
+// username and password
+// aestheticnahid
+// i4Fp6bm97WKty5vz
+
+const uri =
+  "mongodb+srv://aestheticnahid:i4Fp6bm97WKty5vz@users.lcs1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("connected the database");
+
+  const user = { name: "Munna", age: 21, phone: "01827312314" };
+  collection.insertOne(user).then((res) => console.log(res));
+  //   client.close();
+});
+
+app.get("/", (req, res) =>
+  res.send({ status: `"Server is running successfully on port ${port}"` })
+);
+app.listen(port, () => console.log(`server is running on port ${port}`));
