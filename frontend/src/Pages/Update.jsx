d78de33dbc7bd3758f2ec9d007ba01a258c214ab
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-23 02:52:11 +0600

    added update detail page with dynamic users'

--- frontend/src/Pages/Update.jsx
+++ frontend/src/Pages/Update.jsx
@@ -1,7 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 export default function Update() {
+  const { id } = useParams();
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    axios(`http://localhost:5000/users/${id}`).then((res) => {
+      setUser(res.data);
+    });
+  }, [id]);
   return (
-    <>
-      <h1>Update</h1>
-    </>
+    <div className="container mt-5">
+      <h1>Update of {user.name}</h1>
+      <div className="bg-warning p-3 rounded ">
+        <h1>{user.email}</h1>
+        <h1>{user.phone}</h1>
+      </div>
+    </div>
   );
 }
