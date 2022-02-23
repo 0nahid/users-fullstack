1b154c1d36b37738074a9ed6ca93ecb013024c5d
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-23 17:07:34 +0600

    added update user page

--- frontend/src/Pages/Update.jsx
+++ frontend/src/Pages/Update.jsx
@@ -1,20 +1,60 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 export default function Update() {
+  const [user, setUser] = useState({ name: "", email: "", phone: "" });
   const { id } = useParams();
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
   useEffect(() => {
     axios(`http://localhost:5000/users/${id}`).then((res) => {
       setUser(res.data);
     });
   }, [id]);
+  const handleUpdateUser = (e) => {
+    e.preventDefault();
+    axios.put(`http://localhost:5000/users/${id}`, user).then((res) => {
+      if (res.data.n === 1) {
+        Swal.fire("Good job!", "User updated!", "success");
+      } else {
+        Swal.fire("Oops!", "Something went wrong!", "error");
+      }
+    });
+  };
   return (
     <div className="container mt-5">
       <h1>Update of {user.name}</h1>
       <div className="bg-warning p-3 rounded ">
-        <h1>{user.email}</h1>
-        <h1>{user.phone}</h1>
+        {/* <h1>{user.email}</h1>
+        <h1>{user.phone}</h1> */}
+        <form onSubmit={handleUpdateUser}>
+          <input
+            type="text"
+            name=""
+            value={user.name || ""}
+            className="form-control mt-2"
+            id=""
+          />
+          <input
+            type="email"
+            name=""
+            value={user.email || ""}
+            className="form-control mt-2"
+            id=""
+          />
+          <input
+            type="number"
+            name=""
+            value={user.phone || ""}
+            className="form-control mt-2"
+            id=""
+          />
+          <input
+            type="submit"
+            className="btn btn-primary mt-2"
+            value="Update user"
+          />
+        </form>
       </div>
     </div>
   );
